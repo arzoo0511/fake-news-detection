7908aa595da00fbcd225a85c44c044703c4233ff
arzoo0511 <148741922+arzoo0511@users.noreply.github.com>
2024-11-07 11:32:37 +0530

    preprocessing.py

--- src/preprocessing.py
+++ src/preprocessing.py
@@ -1,1 +1,13 @@
+import pandas as pd
+import re
+
+def clean_text(text):
+    text = re.sub(r'\W', ' ', text)       # Remove special characters
+    text = re.sub(r'\s+', ' ', text)      # Remove extra spaces
+    return text.lower()
+
+def load_and_preprocess(path):
+    data = pd.read_csv(path)
+    data['text'] = data['text'].apply(clean_text)
+    return data
 
